1aed53dbb5ae9738346ef360dc069bb756d21ea7
SunehaMariam <sunehamariam475@gmail.com>
2024-12-27 17:28:50 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-const toggleButton = document.getElementById("toggleButton");
+let toggleButton = document.getElementById("toggleButton");
 toggleButton.onclick=() =>{
     if (document.body.style.backgroundColor==="white") {
         document.body.style.backgroundColor="black";
-        toggleButton.textContent="switch to light mode"
+        toggleButton.textContent="switch to light mode 🔆"
+        document.body.style.backgroundcolor="grey";
         document.body.style.color="white"
     } else {
         document.body.style.backgroundColor="white"
-        toggleButton.textContent="switch to dark mode";
+        toggleButton.textContent="switch to dark mode ☀︎";
         document.body.style.color="black";
+        document.body.style.backgroundcolor="white";
        
     }
 }
