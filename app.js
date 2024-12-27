64557b6e66c7f988762a10f5b9532ba04e93a46f
SunehaMariam <sunehamariam475@gmail.com>
2024-12-27 18:03:54 +0500

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ toggleButton.onclick=() =>{
     if (document.body.style.backgroundColor==="white") {
         document.body.style.backgroundColor="black";
         toggleButton.textContent="switch to light mode 🔆"
-        document.body.style.backgroundcolor="grey";
-        document.body.style.color="white"
+   
     } else {
         document.body.style.backgroundColor="white"
         toggleButton.textContent="switch to dark mode ☀︎";
